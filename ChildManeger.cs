9bfa557ad74a28d369de7d38dad7fb820ca1a15e
JoeyG0 <34190161+JoeyG0@users.noreply.github.com>
2018-04-20 16:14:35 -0400

    A script that, produces an array with refrences to the game object child game objects, as long as they have a transform component

---
+++ ChildManeger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildManeger : MonoBehaviour {
+
+    public GameObject[] children;
+    public int transformCount;
+    public int counter;
+	// Use this for initialization
+	void Start () {
+        transformCount = transform.childCount;
+        children = new GameObject[transformCount];
+        counter = 0;
+        foreach(Transform i in gameObject.transform) {
+            children[counter] = i.gameObject;
+            counter++;
+        }
+        
+
+	}
+	
+	
+}
